ed5d2b7a1eaac58c78f79bc2a5e5f2a5a94a5abf
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-26 10:39:29 +0530

    Update Flutter.dart

--- Flutter.dart
+++ Flutter.dart
@@ -1,5 +1,16 @@
-5 steps to be successful Android developer:
+----------------------------------------------------------------------------------------------
+Flutter
+----------------------------------------------------------------------------------------------
+https://flutter.dev/
+https://docs.flutter.dev/get-started/install/linux 
 
+Developed by Google in 2017 
+Dart language, the learning curve is high , it takes time to understand the language.
+
+
+---------------------------------------------
+5 steps to be successful Android developer
+---------------------------------------------
 Search the trends from Google Play Store
 Rx Java observer 
 Retrofit http request
